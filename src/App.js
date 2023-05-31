6b4105dba9f30b6ab4cca0df00f15c0a2c259457
Sabal Sinha <sabalsinha20@gmail.com>
2023-05-31 14:59:14 +0530

    frontend part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./component/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className=" bg-blue-400 fixed  w-full z-20 top-0 left-0 border-b border-gray-200  dark:border-gray-600">
+        <div class="max-w-screen-xl text-center flex flex-wrap items-center justify-between  p-6 ">
+          <h1 className=" text-center ">Email Checker</h1>
+        </div>
+      </nav>
+
+      <div className="flex   items-center justify-center mt-20   ">
+        <div className="  flex flex-col items-center justify-center bg-slate-200 box-border h-96 w-96 p-4 border-6 rounded-lg gap-4">
+          <div>
+            <h3 className=" underline ">Enter Email</h3>
+          </div>
+          <div>
+            <input type="email" placeholder="enter@mail.com" />
+          </div>
+          <div className=" ">
+            <button className=" px-2.5 py-2  bg-blue-400 rounded-lg   ">
+              Check
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="Footer">I am Footer</div>
+      {/* </header> */}
+      <div>
+        <HomePage></HomePage>
+        {/* <Service></Service> */}
+      </div>
+    </>
   );
 }
 
